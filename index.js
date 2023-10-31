fd272b9c3eec5df94885370bce4ff3b53da837e7
Ricardo Merlin <rcrdmerlin@gmail.com>
2023-10-31 16:30:14 -0400

    uploading first class functions lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(array) {
+    const newArray = [...array.slice(0,2)]
+    return newArray;
+}
+
+const returnLastTwoDrivers = function(array) {
+    const newArray = array.slice(-2)
+    return newArray;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+    return function(multiple) {
+        return fare*multiple
+    }
+}
+
+
+
+const fareDoubler = function(multiple) {
+    return createFareMultiplier(2)(multiple)
+}
+
+const fareTripler = function(multiple) {
+    return createFareMultiplier(3)(multiple)
+}
+
+
+function selectDifferentDrivers(array, func){
+    return func(array);
+}
+
